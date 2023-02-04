582f9c1b20e8443f06567fef8a69fcefc9f3900e
dkasprzak <dominik.kasprzak1408@gmail.com>
2023-02-04 15:13:49 +0100

    Initial commit

---
+++ src/MySpot.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySpot.Api.Models;
+
+namespace MySpot.Api.Controllers;
+
+[ApiController]
+[Route("reservations")]
+public class ReservationsController : ControllerBase
+{
+    private int _id = 1;
+    private readonly List<Reservation> _reservations = new();
+
+    private readonly List<string> _parkingSpotName = new()
+    {
+        "P1", "P2", "P3", "P4", "P5"
+    };
+
+    [HttpGet]
+    public void Get()
+    {
+    }
+    
+    [HttpPost]
+    public void Post(Reservation reservation)
+    {
+        if (_parkingSpotName.All(x => x != reservation.ParkingSpotName))
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return;
+        }
+
+        var reservationAlreadyExists = _reservations.Any(x =>
+            x.ParkingSpotName == reservation.ParkingSpotName &&
+            x.Date.Date == reservation.Date.Date);
+
+        if (reservationAlreadyExists)
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return;
+        }
+        
+        reservation.Id = _id;
+        reservation.Date = DateTime.UtcNow.AddDays(1);
+        _id++;
+        
+        _reservations.Add(reservation);
+        
+    }
+}
